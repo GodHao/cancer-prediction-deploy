a349c68ad3a273e5bfa446d8698946ec527209b2
AIW015DADM <nitish.michael@aiware.ai>
2020-01-02 09:30:25 +0530

    final commit

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
-def model():
-    df=pd.read_csv("input.csv")
+def model(arg):
+    df=pd.read_csv(arg)
     inter = np.array(df)
     features=inter[:,:7]
     inter3=inter[:,7:8]
@@ -32,9 +32,12 @@ def index():
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1, 7)
     print(to_predict)
-    loaded_model = model()
-    result = loaded_model.predict(to_predict)
-    return result[0]
+    meta = model("input1.csv")
+    lymph = model("input2.csv")
+
+    result1 = meta.predict(to_predict)
+    result2 = lymph.predict(to_predict)
+    return result1[0], result2[0]
 
 
 @app.route('/result', methods=['POST'])
@@ -42,13 +45,18 @@ def result():
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
         to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(float, to_predict_list))
-        result = ValuePredictor(to_predict_list)
+        pr = to_predict_list[0]
+        to_predict_list = list(map(float, to_predict_list[1:]))
+        result1, result2 = ValuePredictor(to_predict_list)
 
-        if int(result) == 1:
-            prediction = 'Metastasis'
+        if int(result1) == 1:
+            prediction1 = 'Metastasis'
         else:
-            prediction = 'Non Metastasis'
+            prediction1 = 'Non Metastasis'
+        if int(result2) == 1:
+            prediction2 = 'lymph'
+        else:
+            prediction2 = 'Non lymph'
 
-        return render_template("result.html", prediction=prediction)
+        return render_template("result.html", prediction1=prediction1,prediction2=prediction2,mrno=pr)
 
